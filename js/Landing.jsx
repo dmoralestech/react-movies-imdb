767cfba16e6adf26a3e5c01df58af832cafe29e8
darwinmorales <darwin.morales.tech@gmail.com>
2017-08-15 21:13:59 +1000

    added ShowCards and Details

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Landing = () -> (
-    <div className="landing">
-      <h1>Video</h1>
-      <input type="text" placeholder="Search"/>
-      <a> or Browse All</a>
-    </div>
+const Landing = () => (
+  <div className="landing">
+    <h1>svideo</h1>
+    <input type="text" placeholder="Search" />
+    <Link to="/search">or Browse All</Link>
+  </div>
 );
 
 export default Landing;
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,3 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ShowCard from './ShowCard';
 
-export default Search = () => <h1>Search Temp</h1>
+class Search extends Component {
+  state = {
+    searchTerm: ''
+  };
+  handleSearchTermChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {this.props.shows
+            .filter(
+              show =>
+              `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;
